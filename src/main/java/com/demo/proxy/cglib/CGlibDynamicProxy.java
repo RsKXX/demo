37b057e5b3fef1872971144a8eb80151c1a27891
ruanshaokang <ruanshaokang55@dingtalk.com>
2021-08-10 17:52:25 +0800

    cglib实现动态代理

---
+++ src/main/java/com/demo/proxy/cglib/CGlibDynamicProxy.java
@@ -0,0 +1,28 @@
+package com.demo.proxy.cglib;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Author RuanShaoKang
+ * @since 2021/8/10 17:13
+ */
+public class CGlibDynamicProxy implements MethodInterceptor {
+    private Enhancer enhancer = new Enhancer();
+    public Object getProxy(Class clazz){
+        //设置需要创建子类的类
+        enhancer.setSuperclass(clazz);
+        enhancer.setCallback(this);
+        //通过字节码技术动态创建子类实例
+        return enhancer.create();
+    }
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("cglib  前置");
+        Object res = methodProxy.invokeSuper(o, objects);
+        return res;
+    }
+}
--- src/test/java/com/example/demo/DynamicProxyTest.java
+++ src/test/java/com/example/demo/DynamicProxyTest.java
@@ -1,21 +1,29 @@
 package com.example.demo;
 
 import com.demo.Application;
+import com.demo.proxy.cglib.CGlibDynamicProxy;
 import com.demo.proxy.jdk.JDKDynamicProxy;
 import com.demo.proxy.jdk.RealSubject;
 import com.demo.proxy.jdk.Subject;
 import com.demo.proxy.jdk.Subject1;
-import com.demo.service.NewService;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = Application.class)
 public class DynamicProxyTest {
 
-    @org.junit.jupiter.api.Test
-    void test(){
+    @Test
+    void jdk(){
         Subject proxy = new JDKDynamicProxy(new RealSubject()).getProxy();
         proxy.pr();
         Subject1 proxy1 = (Subject1) proxy;
         proxy1.pr1();
+    }
+    @Test
+    void cglib(){
+        CGlibDynamicProxy proxy = new CGlibDynamicProxy();
+        RealSubject proxy1 = (RealSubject)proxy.getProxy(RealSubject.class);
+        proxy1.pr1();
+        proxy1.pr();
     }
 }
